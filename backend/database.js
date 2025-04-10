1578af059902a1c0c1b3151b6074b882763021a9
Nutty-Nuts <vegagerard1130@gmail.com>
2025-04-10 20:12:37 +0800

    INIT: Finished initializing configuration files for Docker and components (for now...)

--- backend/database.js
+++ backend/database.js
@@ -1,13 +1,21 @@
 const { Pool } = require("pg");
 const fs = require("fs");
 
-const pool = new Pool({
-  port: process.env.POSTGRES_PORT,
-  user: process.env.POSTGRES_USER,
-  password: fs.readFileSync(process.env.POSTGRES_PASSWORD_FILE),
-  database: process.env.POSTGRES_DB,
-});
+const {
+  POSTGRES_USER: USER,
+  POSTGRES_USER_FILE: USER_FILE,
+  POSTGRES_PASSWORD: PASSWORD,
+  POSTGRES_PASSWORD_FILE: PASSWORD_FILE,
+  POSTGRES_DB: DB,
+  POSTGRES_DB_FILE: DB_FILE,
+  POSTGRES_PORT: PORT,
+} = process.env;
 
-console.log(process.env.POSTGRES_PORT);
+const pool = new Pool({
+  port: PORT,
+  user: USER_FILE ? fs.readFileSync(USER_FILE) : USER,
+  password: PASSWORD_FILE ? fs.readFileSync(PASSWORD_FILE) : PASSWORD,
+  database: DB_FILE ? fs.readFileSync(DB_FILE) : DB,
+});
 
 module.exports = pool;
